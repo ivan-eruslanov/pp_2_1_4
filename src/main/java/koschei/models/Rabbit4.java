dad82263fed228df5222359673440eeeaa8da15e
eruslanov-ivan <iv.eruslanov@yandex.ru>
2022-08-02 19:06:13 +0500

    fixed class Egg6

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -5,15 +5,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
-    private Duck5 duck5;
+    private Duck5 duck;
 
     @Autowired
-    public void setDuck5(Duck5 duck5) {
-        this.duck5 = duck5;
+    public void setDuck(Duck5 duck) {
+        this.duck = duck;
     }
 
     @Override
     public String toString() {
-        return ", в зайце утка " + duck5.toString();
+        return ", в зайце утка " + duck.toString();
     }
 }
